c4e6905182898ecbc8890667d66546513443d4be
Daniel Nichols <denichols297@macbookpro.lan>
2026-02-28 20:04:58 -0500

    Fix WebKit webview cache to pull latest backend changes

--- app.py
+++ app.py
@@ -59,6 +59,13 @@ def get_cached_or_fetch_news():
     with cache_lock:
         return news_cache.get("data", {})
 
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
 @app.route('/')
 def index():
     # Instantly return the pure HTML shell
